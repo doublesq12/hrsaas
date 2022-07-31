c5b55e2586604b07f038a5d594a81e317e2c7254
doublekou <951513186@qq.com>
2022-07-31 10:10:14 +0800

    完成项目的基本配置

--- src/icons/index.js
+++ src/icons/index.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import SvgIcon from '@/components/SvgIcon'// svg component
+import SvgIcon from '@/components/SvgIcon' // svg component
 
 // 注册全局组件svg-icon
 // register globally
@@ -7,5 +7,5 @@ Vue.component('svg-icon', SvgIcon)
 
 // 一次性加载所有的/svg目录下的所有svg文件
 const req = require.context('./svg', false, /\.svg$/)
-const requireAll = requireContext => requireContext.keys().map(requireContext)
+const requireAll = (requireContext) => requireContext.keys().map(requireContext)
 requireAll(req)
