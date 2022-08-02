87247458e0df49076b4f3b928147663e8a29c941
doublekou <951513186@qq.com>
2022-08-02 14:57:45 +0800

    完成sidebar样式修改

---
+++ src/permission.js
@@ -0,0 +1,21 @@
+import router from '@/router'
+import store from '@/store'
+//路由全局前置首位
+const whiteList=['/login','/404']
+router.beforeEach((to, from, next) => {
+  const token = store.state.user.token
+  if (token) {
+    if (to.path === '/login') {
+      next('/')
+    } else {
+      next()
+    }
+  } else {
+    const isCludes = whiteList.includes(to.path)
+    if (isCludes) {
+      next()
+    } else {
+      next('/login')
+    }
+  }
+})
